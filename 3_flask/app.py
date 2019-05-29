095e5455b8b1632802a492279a1c1b0f992d9d7c
neo <neo@hphk.kr>
2019-05-29 15:57:56 +0900

    make functions

--- 3_flask/app.py
+++ 3_flask/app.py
@@ -42,40 +42,15 @@ def get_lotto(num):
 
 @app.route('/lotto/<int:num>')
 def lotto(num):
-    lucky_numbers = random.sample(range(1, 46), 6)
-    lucky_numbers.sort()
+    lucky_numbers = get_random_numbers()
 
-    url = f'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo={num}'
-    res = requests.get(url).text  # type == String
-    data = json.loads(res)  # type == dict
-    bonus_number = data['bnusNo']
+    real_data = get_lotto_numbers(num)  # { 'real': [1,2,3,4,5,6], 'bonus': 7 }
 
-    real_numbers = []
-
-    if data['returnValue'] == 'success':
-        for key, value in data.items():
-            if 'drwtNo' in key:
-                real_numbers.append(value)
-
-        real_numbers.sort()
+    real_numbers = real_data['real']
+    bonus_number = real_data['bonus']
 
     # 등수 비교
-    lucky = set(lucky_numbers)
-    real = set(real_numbers)
 
-    match_count = len(real.intersection(lucky))
-
-    result = '꽝'
-    if match_count == 6:
-        result = 1
-    elif match_count == 5 and bonus_number in lucky_numbers:
-        result = 2
-    elif match_count == 5:
-        result = 3
-    elif match_count == 4:
-        result = 4
-    elif match_count == 3:
-        result = 5
 
     return render_template(
         'lotto.html',
@@ -90,6 +65,49 @@ def lotto(num):
 def square(num):
     result = num ** 2
     return f'{result}'
+
+
+def get_random_numbers():
+    numbers = random.sample(range(1, 46), 6)
+    return sorted(numbers)
+
+
+def get_lotto_numbers(num):
+    url = f'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo={num}'
+    res = requests.get(url).text  # type == String
+    data = json.loads(res)  # type == dict
+    bonus_number = data['bnusNo']
+
+    real_numbers = []
+
+    if data['returnValue'] == 'success':
+        for key, value in data.items():
+            if 'drwtNo' in key:
+                real_numbers.append(value)
+
+        real_numbers.sort()
+    return {'real': real_numbers, 'bonus': bonus_number}
+
+
+def get_result(real_list, random_list, bonus):
+    lucky = set(real_list)
+    real = set(random_list)
+
+    match_count = len(real.intersection(lucky))
+
+    result = '꽝'
+    if match_count == 6:
+        result = 1
+    elif match_count == 5 and bonus in random_list:
+        result = 2
+    elif match_count == 5:
+        result = 3
+    elif match_count == 4:
+        result = 4
+    elif match_count == 3:
+        result = 5
+
+    return result
 
 
 if __name__ == '__main__':
