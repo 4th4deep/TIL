21c3d35f9bbe550125741944417701647ea0ee21
neo <neo@hphk.kr>
2019-05-29 14:35:30 +0900

    get_lotto, pick_lotto

--- 3_flask/app.py
+++ 3_flask/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
+import requests
 import random
+import json
 
 app = Flask(__name__)
 
@@ -16,10 +18,28 @@ def pick_lotto():
     return render_template('pick_lotto.html', numbers=lucky_numbers)
 
 
-@app.route('/lotto/<int:num>')
-def find_lotto(num):
+@app.route('/get_lotto/<int:num>')
+def get_lotto(num):
+    url = f'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo={num}'
+    res = requests.get(url).text  # type == String
+    data = json.loads(res)  # type == dict
 
-    return num
+    real_numbers = []
+
+    if data['returnValue'] == 'success':
+        for key, value in data.items():
+            if 'drwtNo' in key:
+                real_numbers.append(value)
+
+        real_numbers.sort()
+
+    return render_template(
+        'get_lotto.html',
+        numbers=real_numbers,
+        draw_no=num
+    )
+
+
 
 
 @app.route('/square/<int:num>')
