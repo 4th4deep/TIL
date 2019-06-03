fbcd6c8f5c7bb3076b55c74688cdf3c8643af883
neo <neo@hphk.kr>
2019-06-03 14:15:39 +0900

    structure django app

--- 4_django/FIRST_DJANGO/first_django/urls.py
+++ 4_django/FIRST_DJANGO/first_django/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from . import views
-
+from . import views as master_views
+from home import views as home_views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', views.index),
-    path('hello/<str:name>/', views.hello)
+    # path('index/', views.index),
+    # path('hello/<str:name>/', views.hello),
+    path('home/', home_views.home_index)
 ]
