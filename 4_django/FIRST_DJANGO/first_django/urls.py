de951eb1152bbfe1625b5433179a9868b1e04a45
neo <neo@hphk.kr>
2019-06-03 14:54:53 +0900

    seperate URLs

--- 4_django/FIRST_DJANGO/first_django/urls.py
+++ 4_django/FIRST_DJANGO/first_django/urls.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from . import views as master_views
-from home import views as home_views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('index/', views.index),
-    # path('hello/<str:name>/', views.hello),
-    path('home/', home_views.home_index)
+    path('home/', include('home.urls')),
 ]
