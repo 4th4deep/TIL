e0be4f4966d344de897f6ad5c12db633316c6fa4
neo <neo@hphk.kr>
2019-06-04 11:19:26 +0900

    django user input ing

--- 4_django/FIRST_DJANGO/utils/urls.py
+++ 4_django/FIRST_DJANGO/utils/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index),  # utils/
     path('art/<str:keyword>/', views.artii),  # utils/art/<KEYWORD>
-    path('stock/', views.stock),  # utils/stock/
+    path('stock_input/', views.stock_input),  # utils/stock_input/
+    path('stock_output/', views.stock_output),  # utils/stock_output/
 ]
