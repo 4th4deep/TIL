004b881164b13aa3f88811568ceb90cf1cc0f952
neo <neo@hphk.kr>
2019-06-12 11:53:08 +0900

    create

--- 4_django/instagram/posts/views.py
+++ 4_django/instagram/posts/views.py
@@ -6,13 +6,29 @@ def index(request):
     return render(request, 'posts/index.html')
 
 def create(request):
+    # 1. get방식으로 데이터를 입력할 수 있는 form을 요청한다.
+    # 4. 사용자가 데이터를 입력해서 post 요청을 보낸다.
+    # 9. 사용자가 적절한데이터를 입력해서 post 요청을 보낸다.
     if request.method == "POST":
+        # 5. 데이터를 받아서 PostForm을 인스턴스화 한다.
+        # 10. 데이터를 받아서 PostForm을 인스턴스화 한다.
         form = PostForm(request.POST)
-        form.save()
-        return redirect("posts:index")
+        # 6. 데이터 검증을 한다.
+        # 11. 데이터 검증을 한다.
+        if form.is_valid():
+            # 12. 적절한 데이터가 들어온다. 저장을 하고 인덱스로 보낸다.
+            form.save()
+            return redirect("posts:index")
+        else:
+            # 7. 적절하지 않은 데이터가 들어온다.
+            pass
     else:
+        # 2. PostForm을 인스턴스화 해서 form 변수에 저장
         form = PostForm()
-        context = {
-            'form':form
-        }
-        return render(request, 'posts/create.html', context)
+
+    context = {
+        'form':form
+    }
+    # 3. 만들어진 form을 create.html에 담아서 전송
+    # 8. 사용자가 정확하게 입력한 데이터를 유지한 상태의 form을 전송
+    return render(request, 'posts/create.html', context)
